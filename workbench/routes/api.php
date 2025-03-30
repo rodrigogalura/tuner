37a99e3e396c7be7c2d4a2c4fcda3fdc3bb1571d
Rodrigo Galura <rodrigogalura3rd@gmail.com>
2025-03-30 09:24:12 +0800

    remove workbench folder and add .gitattributes file

--- workbench/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Workbench\App\Http\Controllers\UserController;
-
-Route::get('/api/users', [UserController::class, 'index'])->name('api.users.index');
