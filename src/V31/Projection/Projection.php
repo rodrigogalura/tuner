16ee6040abc6e699cad233e3c1dc274f552b2393
Rodrigo Galura <rodrigogalura3rd@gmail.com>
2025-08-04 03:25:33 +0800

    to be continue projection truthtable relation

--- src/V31/Projection/Projection.php
+++ src/V31/Projection/Projection.php
@@ -2,7 +2,41 @@
 
 namespace Laradigs\Tweaker\V31\Projection;
 
+use Laradigs\Tweaker\V31\TruthTable\TruthTable;
+
 class Projection
 {
+    private TruthTable $truthTable;
 
+    public function __construct(array $columns)
+    {
+        $this->truthTable = new TruthTable($columns);
+    }
+
+    public function exportToCSV($filename, array $variables)
+    {
+        $handle = fopen($filename, 'w');
+
+        fputcsv($handle, ['Truth Table']);
+        fputcsv($handle, array_merge(
+            array_keys($variables),
+            ['Intersect', 'Intersect Strict', 'Except', 'Except Strict']
+        ));
+
+        $matrix = $this->matrix2d($variables);
+
+        foreach ($matrix as $m) {
+            if (isset($current) && $current !== $m[0]) {
+                fputcsv($handle, []);
+            }
+
+            $current = $m[0];
+            fputcsv($handle, $m);
+        }
+
+        fclose($handle);
+        if (file_exists($filename)) {
+            echo "CSV file created successfully: {$filename}".PHP_EOL;
+        }
+    }
 }
--- src/V31/TruthTable/TruthTable.php
+++ src/V31/TruthTable/TruthTable.php
@@ -2,23 +2,53 @@
 
 namespace Laradigs\Tweaker\V31\TruthTable;
 
-use function RGalura\ApiIgniter\is_multi_array;
 use function RGalura\ApiIgniter\filter_explode;
 
 class TruthTable
 {
     public function __construct(
-        private array $rules = [],
+        // private array $rules = [],
         private array $asteriskValues = []
     )
     {
         // $this->variables = $variables;
     }
 
-    private function someFirstNotInSecond(array $first, array $second)
+    private function extractIfAsterisk(string &$str)
     {
-        return ! empty (array_diff($first, $second));
+        if (trim($str) === '*') {
+            $str = implode(', ', $this->asteriskValues);
+        }
     }
+
+    protected function intersectAllKeys(array $keys)
+    {
+        while (count($keys) > 1) {
+            $p = filter_explode(array_shift($keys));
+            $q = filter_explode(array_shift($keys));
+
+            $keys[0] = implode(', ', $this->intersect($p, $q));
+
+            next($keys);
+        }
+
+        return array_shift($keys);
+    }
+
+    public function intersect(array $p, array $q)
+    {
+        return array_values(array_intersect($p, $q));
+    }
+
+    public function except(array $p, array $q)
+    {
+        return array_values(array_diff($p, $q));
+    }
+
+    // private function someFirstNotInSecond(array $first, array $second)
+    // {
+    //     return ! empty (array_diff($first, $second));
+    // }
 
     // private function intersectStrict(array $p, array $q, int $errorCode)
     // {
@@ -33,27 +63,6 @@ class TruthTable
     //         ? [$errorCode]
     //         : $this->except($p, $q);
     // }
-
-    private function extractIfAsterisk(string &$str)
-    {
-        if (trim($str) === '*') {
-            $str = implode(', ', $this->asteriskValues);
-        }
-    }
-
-    private function intersectAllKeys(array $keys)
-    {
-        while (count($keys) >= 2) {
-            $p = filter_explode(array_shift($keys));
-            $q = filter_explode(array_shift($keys));
-
-            $keys[0] = implode(', ', $this->intersect($p, $q));
-
-            next($keys);
-        }
-
-        return array_shift($keys);
-    }
 
     public function matrix2d(array $variables)
     {
@@ -78,42 +87,5 @@ class TruthTable
         }
 
         return array_shift($variables);
-    }
-
-    public function export($filename, array $variables)
-    {
-        $handle = fopen($filename, 'w');
-
-        fputcsv($handle, ['Truth Table']);
-        fputcsv($handle, array_merge(
-            array_keys($variables),
-            ['Intersect', 'Intersect Strict', 'Except', 'Except Strict']
-        ));
-
-        $matrix = $this->matrix2d($variables);
-
-        foreach ($matrix as $m) {
-            if (isset($current) && $current !== $m[0]) {
-                fputcsv($handle, []);
-            }
-
-            $current = $m[0];
-            fputcsv($handle, $m);
-        }
-
-        fclose($handle);
-        if (file_exists($filename)) {
-            echo "CSV file created successfully: {$filename}".PHP_EOL;
-        }
-    }
-
-    public function intersect(array $p, array $q)
-    {
-        return array_values(array_intersect($p, $q));
-    }
-
-    public function except(array $p, array $q)
-    {
-        return array_values(array_diff($p, $q));
     }
 }
